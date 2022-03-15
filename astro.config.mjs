7e4c448113ac6f90b9838e14a77f78bb7b6e9c6e
David Brown <d.brown@peoplebank.com>
2022-03-15 15:39:49 +0000

    updated Solutions page

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,8 @@
 export default /** @type {import('astro').AstroUserConfig} */ ({
   // Comment out "renderers: []" to enable Astro's default component support.
   renderers: [],
+  buildOptions: {
+    sitemap: true,
+    site: 'https://pb4demo.peoplebank.com/pb3/corporate/peoplebank/'
+  },
 });
