ad80aa2be2f4e0d222caa3296ddebf82c088cc67
David Brown <d.brown@peoplebank.com>
2022-04-22 17:03:27 +0100

    Added nav dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-console.log("%c ¯\\_(ツ)_/¯", "font-size: 2rem")
+console.log("%c ¯\\_(ツ)_/¯", "font-size: 2rem");
+
+let currentPath = window.location.href;
+let navLinks = document.querySelectorAll(".nav-list a");
+
+navLinks.forEach(function (element) {
+  if (element.href == currentPath) {
+    element.classList.add("active");
+  }
+});
