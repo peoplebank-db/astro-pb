21ecc3cfba0a0e695bbab0c79bb50eb680db11cb
David Brown <d.brown@peoplebank.com>
2023-06-30 16:52:22 +0100

    updated to latest astro and fixed everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Comment out "renderers: []" to enable Astro's default component support.
-  renderers: [],
-  buildOptions: {
-    sitemap: true,
-    site: 'https://pb4demo.peoplebank.com/pb3/corporate/peoplebank/'
-  },
+export default defineConfig({
+  base: '/pb3/corporate/peoplebank/',
+  site: 'https://pb4demo.peoplebank.com/pb3/corporate/peoplebank/'
 });
